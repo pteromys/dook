79a2fe5394c3a3a29f226e4e31fa60a35964b2d3
pteromys <pteromys@gmail.com>
2025-05-03 12:12:57 -0400

    Switch test line numbers to what bat displays.
    Eliminate manual conversion as a source of mistakes.

--- tests/examples.rs
+++ tests/examples.rs
@@ -40,7 +40,11 @@ fn verify_examples(language_name: LanguageName, source: &[u8], cases: &[TestCase
         let pattern = regex::Regex::new(&pattern_str).unwrap();
         let search_result =
             searches::find_definition(source, &tree, &language_info, &pattern, true);
-        let result_vec: Vec<_> = search_result.ranges.iter().collect();
+        let result_vec: Vec<_> = search_result
+            .ranges
+            .iter()
+            .map(|r| r.start + 1..r.end)
+            .collect();
         assert_eq!(
             result_vec, *expect_ranges,
             "searching {:?} for {:?} got {:?}, expected {:?}",
@@ -90,7 +94,7 @@ fn verify_multipass_examples(
             &mut query_compiler,
         )
         .unwrap();
-        let result_vec: Vec<_> = result.ranges.iter().collect();
+        let result_vec: Vec<_> = result.ranges.iter().map(|r| r.start + 1..r.end).collect();
         assert_eq!(
             result_vec, *expect_ranges,
             "searching {:?} for {:?} got {:?}, expected {:?}",
@@ -101,37 +105,37 @@ fn verify_multipass_examples(
 
 #[test]
 fn python() {
-    // these ranges are 0-indexed and bat line numbers are 1-indexed so generate them with `nl -ba -v0`
+    // these ranges are 1-indexed and include both ends
     #[rustfmt::skip]
     let cases = [
-        ("one", vec![11..34], vec!["hecks"]), // hm I don't like this
-        ("two", vec![13..15], vec![]),
-        ("three", vec![13..14, 15..16], vec![]),
-        ("four", vec![13..14, 17..24], vec![]),
-        ("five", vec![13..14, 21..22], vec![]),
-        ("six", vec![13..14, 25..34], vec!["hecks"]),
-        ("seven", vec![40..47], vec![]),
-        ("eight", vec![48..49], vec![]),
+        ("one", vec![12..34], vec!["hecks"]), // hm I don't like this
+        ("two", vec![14..15], vec![]),
+        ("three", vec![14..14, 16..16], vec![]),
+        ("four", vec![14..14, 18..24], vec![]),
+        ("five", vec![14..14, 22..22], vec![]),
+        ("six", vec![14..14, 26..34], vec!["hecks"]),
+        ("seven", vec![41..47], vec![]),
+        ("eight", vec![49..49], vec![]),
         // nine and ten are function parameters split across multiple lines;
         // I assume you want the whole signature because it'll be either short enough to not be a pain
         // or long enough to need further clarification if you only see one line from it.
-        ("nine", vec![13..14, 26..33], vec![]),
-        ("ten", vec![13..14, 26..33], vec![]),
+        ("nine", vec![14..14, 27..33], vec![]),
+        ("ten", vec![14..14, 27..33], vec![]),
         ("int", vec![], vec![]),
-        ("abc", vec![43..45], vec![]),
-        ("xyz", vec![43..44, 45..46], vec![]),
-        ("def", vec![51..53], vec![]),
-        ("factorial", vec![55..57], vec!["permutations"]),
-        ("permutations", vec![59..63], vec!["permutations"]),
-        ("combinations", vec![65..67], vec!["factorial", "permutations"]),
-        ("combinations2", vec![69..71], vec!["factorial"]),
-        ("attr", vec![73..78], vec!["__setattr__", "__setitem__", "setattr"]),
-        ("eleven", vec![82..84], vec![]),
-        ("twelve", vec![82..84], vec![]),
-        ("thirteen", vec![82..83, 85..86], vec![]),
-        ("fourteen", vec![82..83, 89..92], vec![]),  // 2nd group should be 88..92
-        ("fifteen", vec![82..83, 93..94], vec![]),
-        ("sixteen", vec![96..100], vec![]),
+        ("abc", vec![44..45], vec![]),
+        ("xyz", vec![44..44, 46..46], vec![]),
+        ("def", vec![52..53], vec![]),
+        ("factorial", vec![56..57], vec!["permutations"]),
+        ("permutations", vec![60..63], vec!["permutations"]),
+        ("combinations", vec![66..67], vec!["factorial", "permutations"]),
+        ("combinations2", vec![70..71], vec!["factorial"]),
+        ("attr", vec![74..78], vec!["__setattr__", "__setitem__", "setattr"]),
+        ("eleven", vec![83..84], vec![]),
+        ("twelve", vec![83..84], vec![]),
+        ("thirteen", vec![83..83, 86..86], vec![]),
+        ("fourteen", vec![83..83, 90..92], vec![]),  // 2nd group should be 89..92
+        ("fifteen", vec![83..83, 94..94], vec![]),
+        ("sixteen", vec![97..100], vec![]),
     ];
     verify_examples(
         LanguageName::PYTHON,
@@ -142,28 +146,28 @@ fn python() {
 
 #[test]
 fn js() {
-    // these ranges are 0-indexed and bat line numbers are 1-indexed so generate them with `nl -ba -v0`
+    // these ranges are 1-indexed and include both ends
     #[rustfmt::skip]
     let cases = [
-        ("one", vec![0..1], vec![]),  // let
-        ("two", vec![1..2], vec![]),  // const
-        ("three", vec![3..6], vec![]),  // function declaration
+        ("one", vec![1..1], vec![]),  // let
+        ("two", vec![2..2], vec![]),  // const
+        ("three", vec![4..6], vec![]),  // function declaration
         // old-style class, prototype shenanigans
-        ("four", vec![7..10, 11..17, 20..23], vec![]),
-        ("f", vec![11..15], vec![]),  // object key, bare
-        ("flop", vec![11..15], vec![]),  // named function expression
-        ("eff", vec![11..12, 15..16], vec![]),  // object key, in quotes
-        ("g", vec![20..23], vec![]),  // assign to dot-property
-        ("five", vec![24..29], vec![]),  // new-style class
-        ("six", vec![24..26], vec![]),  // class member variable
-        ("seven", vec![24..25, 27..28], vec![]),  // getter
-        ("eight", vec![30..31], vec![]),  // function argument
-        ("nine", vec![30..31], vec![]),  // function argument with default
-        ("ten", vec![30..31], vec![]),  // rest parameters
-        ("eleven", vec![32..33], vec![]),  // array destructuring
-        ("twelve", vec![32..33], vec![]),  // array destructuring
-        ("thirteen", vec![33..34], vec![]),  // object destructuring
-        ("fourteen", vec![34..35], vec![]),  // shorthand object destructuring
+        ("four", vec![8..10, 12..17, 21..23], vec![]),
+        ("f", vec![12..15], vec![]),  // object key, bare
+        ("flop", vec![12..15], vec![]),  // named function expression
+        ("eff", vec![12..12, 16..16], vec![]),  // object key, in quotes
+        ("g", vec![21..23], vec![]),  // assign to dot-property
+        ("five", vec![25..29], vec![]),  // new-style class
+        ("six", vec![25..26], vec![]),  // class member variable
+        ("seven", vec![25..25, 28..28], vec![]),  // getter
+        ("eight", vec![31..31], vec![]),  // function argument
+        ("nine", vec![31..31], vec![]),  // function argument with default
+        ("ten", vec![31..31], vec![]),  // rest parameters
+        ("eleven", vec![33..33], vec![]),  // array destructuring
+        ("twelve", vec![33..33], vec![]),  // array destructuring
+        ("thirteen", vec![34..34], vec![]),  // object destructuring
+        ("fourteen", vec![35..35], vec![]),  // shorthand object destructuring
     ];
     for language_name in [
         LanguageName::JAVASCRIPT,
@@ -180,12 +184,12 @@ fn js() {
 
 #[test]
 fn tsx() {
-    // these ranges are 0-indexed and bat line numbers are 1-indexed so generate them with `nl -ba -v0`
+    // these ranges are 1-indexed and include both ends
     #[rustfmt::skip]
     let cases = [
-        ("eight", vec![0..1], vec![]),  // function argument
-        ("nine", vec![0..1], vec![]),  // function argument with default
-        ("ten", vec![0..1], vec![]),  // rest parameters
+        ("eight", vec![1..1], vec![]),  // function argument
+        ("nine", vec![1..1], vec![]),  // function argument with default
+        ("ten", vec![1..1], vec![]),  // rest parameters
     ];
     verify_examples(
         LanguageName::TSX,
@@ -196,23 +200,23 @@ fn tsx() {
 
 #[test]
 fn c() {
-    // these ranges are 0-indexed and bat line numbers are 1-indexed so generate them with `nl -ba -v0`
+    // these ranges are 1-indexed and include both ends
     #[rustfmt::skip]
     let cases = [
-        ("ONE", vec![2..3], vec![]),  // #define
-        ("two", vec![5..6], vec![]),  // static const
-        ("ThreeStruct", vec![7..11], vec![]),  // struct
-        ("Three", vec![7..11], vec![]),  // typedef struct; see https://stackoverflow.com/a/1675446
-        ("THREE_PTR", vec![12..13], vec![]),  // typedef of pointer to struct
-        ("Pint", vec![14..15], vec![]),  // typedef pointer to other stuff
-        ("Quart", vec![16..20], vec![]),  // struct not in a typedef
-        ("four", vec![7..9], vec![]),  // member
-        ("five", vec![7..8, 9..10], vec![]),  // array
-        ("six", vec![21..22], vec![]),  // unreasonable levels of pointer nesting
-        ("SEVEN", vec![23..24, 33..34], vec![]),  // macro
-        ("second_order", vec![25..32], vec![]),  // function definition
-        ("callback", vec![25..30], vec![]),  // function pointer
-        ("right", vec![25..30], vec![]),  // other function parameter
+        ("ONE", vec![3..3], vec![]),  // #define
+        ("two", vec![6..6], vec![]),  // static const
+        ("ThreeStruct", vec![8..11], vec![]),  // struct
+        ("Three", vec![8..11], vec![]),  // typedef struct; see https://stackoverflow.com/a/1675446
+        ("THREE_PTR", vec![13..13], vec![]),  // typedef of pointer to struct
+        ("Pint", vec![15..15], vec![]),  // typedef pointer to other stuff
+        ("Quart", vec![17..20], vec![]),  // struct not in a typedef
+        ("four", vec![8..9], vec![]),  // member
+        ("five", vec![8..8, 10..10], vec![]),  // array
+        ("six", vec![22..22], vec![]),  // unreasonable levels of pointer nesting
+        ("SEVEN", vec![24..24, 34..34], vec![]),  // macro
+        ("second_order", vec![26..32], vec![]),  // function definition
+        ("callback", vec![26..30], vec![]),  // function pointer
+        ("right", vec![26..30], vec![]),  // other function parameter
     ];
     verify_examples(LanguageName::C, include_bytes!("../test_cases/c.c"), &cases);
 }
@@ -220,8 +224,8 @@ fn c() {
 #[test]
 fn markdown_injections() {
     let cases = [
-        ("Nordstrom", vec![3..4, 7..8, 10..11, 12..13, 14..15]),
-        ("spartacus", vec![19..20, 23..25, 27..29]),
+        ("Nordstrom", vec![4..4, 8..8, 11..11, 13..13, 15..15]),
+        ("spartacus", vec![20..20, 24..25, 28..29]),
     ];
     verify_multipass_examples(
         LanguageName::MARKDOWN,
